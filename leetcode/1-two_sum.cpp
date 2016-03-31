e9a4327f36183e692eb6e7e73a78ca04d8838cff
Jefferson Monteiro <jefferson.willian.monteiro@gmail.com>
2016-03-31 00:29:37 -0300

    Add LeetCode Two Sum problem.

---
+++ leetcode/1-two_sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+ public:
+  // Time Complexity  = O(n * h)
+  // Space Complexity = O(n) 
+  //
+  // n : input size
+  // h : hash lookup average complexity. O(1) best case and O(n) worst case.
+  vector<int> twoSum(vector<int>& nums, int target) {
+    // Hash the number value to its index in |nums|.
+    unordered_map<int, int> hash;
+
+    for (int i = 0; i < nums.size(); i++) {
+      // Check if the remaining value of the sum is available in the hash. If
+      // it exists then returns both values indexes.
+      if (hash.find(target - nums[i]) != hash.end())
+        return {hash[target - nums[i]], i};
+      // Hash the number value to its index.
+      hash[nums[i]] = i;
+    }
+
+    // Should never reach here.
+  }
+};
