970963cf6ff3ed6378c10e9082cc48416744f123
Jefferson Monteiro <jefferson.willian.monteiro@gmail.com>
2016-05-19 20:14:07 -0300

    LeetCode: 1 - Two Sum

--- leetcode/1-two_sum.cpp
+++ leetcode/1-two_sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
  public:
   // Time Complexity  = O(n * h)
-  // Space Complexity = O(n) 
+  // Space Complexity = O(n)
   //
   // n : nums size.
   // h : hash lookup average complexity. O(1) best case and O(n) worst case.
@@ -9,15 +9,19 @@ class Solution {
     // Hash the number value to its index in |nums|.
     unordered_map<int, int> hash;
 
-    for (int i = 0; i < nums.size(); i++) {
+    for (int i = 0; i < nums.size(); ++i) {
       // Check if the remaining value of the sum is available in the hash. If
       // it exists then returns both values indexes.
-      if (hash.find(target - nums[i]) != hash.end())
-        return {hash[target - nums[i]], i};
+      auto it = hash.find(target - nums[i]);
+      if (it != hash.end())
+        return {it->second, i};
+
       // Hash the number value to its index.
-      hash[nums[i]] = i;
+      hash.insert({nums[i], i});
     }
 
-    // Should never reach here.
+    // Execution should never reach at this point according to the problem
+    // statement.
+    return vector<int>();
   }
 };
