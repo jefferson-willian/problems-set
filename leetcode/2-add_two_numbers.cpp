af94e7c99bda38ffe38795a012baf1457454f394
Jefferson Monteiro <jefferson.willian.monteiro@gmail.com>
2016-05-23 13:08:58 -0300

    LeetCode: Add Two Numbers

--- leetcode/2-add_two_numbers.cpp
+++ leetcode/2-add_two_numbers.cpp
@@ -1,34 +1,21 @@
 class Solution {
  public:
   // Time Complexity  = O(n + m)
-  // Space Complexity = O(n + m) 
+  // Space Complexity = O(n + m)
   //
   // n : first list size.
   // m : second list size.
   ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-    // First number iterator.
+    // Answer list.
+    ListNode* l3 = nullptr;
+
+    // List iterators.
     ListNode* it1 = l1;
-    // Second number iterator.
     ListNode* it2 = l2;
-    // Answer iterator.
-    ListNode* it3 = nullptr;
+    ListNode* it3 = l3;
 
-    // !Important! I am considering that the list input will have at least one
-    // node. This is the case where 0 is represented by a list with one node
-    // with value 0. If we allow empty lists as input then this will fail.
-    //
-    // Sum the first number of both lists and saves the result in the list
-    // |head| (The head of the list answer).
-    int val = it1->val + it2->val;
-    ListNode* head = new ListNode(val % 10);
-
-    // Keeps the sum carry.
-    int carry = val / 10;
-
-    // Update the iterators.
-    it1 = it1->next;
-    it2 = it2->next;
-    it3 = head;
+    // Holds the carry sum.
+    int carry = 0;
 
     // We have three situations where we have to continue add numbers:
     //      - When the first iterator hasn't reached the end of the list.
@@ -39,19 +26,26 @@ class Solution {
       // reached the end of the lists, otherwise uses 0 to sum.
       int a = it1 ? it1->val : 0;
       int b = it2 ? it2->val : 0;
+
       // Sum both numbers and the carry as well.
       int val = a + b + carry;
+
       // Appends the sum to the answer list.
-      it3->next = new ListNode(val % 10);
+      if (l3 == nullptr)
+        l3 = new ListNode(val % 10);
+      else
+        it3->next = new ListNode(val % 10);
+
       // Update iterators if they haven't reached the end of the lists.
-      if (it1) it1 = it1->next;
-      if (it2) it2 = it2->next;
-      it3 = it3->next;
-      // Updates the carry.
+      it1 = it1 ? it1->next : nullptr;
+      it2 = it2 ? it2->next : nullptr;
+      it3 = it3 ? it3->next : l3;
+
+      // Updates the carry sum.
       carry = val / 10;
     }
 
     // Returns the answer.
-    return head;
+    return l3;
   }
 };
